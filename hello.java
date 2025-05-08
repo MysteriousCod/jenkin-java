a350533225390231ebb04c116b9a2bfa615a46b5
devops <urkuderam9049@gmail.com>
2025-05-08 19:52:07 +0530

    Java code 3rd

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello {
 	public static void main(String args[]) {
 		System.out.println("Hello World");
 		System.out.println("Welcome to C-DAC");
+		System.out.println("Welcome to DITISS");
 	}
 }
